c8cdb61695aae146908bc50412ca642b90f07de7
iftikharbg <iftikharbg@code.edu.az>
2021-10-31 16:45:02 +0400

    Homework

---
+++ Task - Copy/js/script.js
@@ -0,0 +1,57 @@
+1.// let m = document.getElementById("button1");
+// let e = document.getElementById("button2");
+// let v = document.getElementById("button3");
+// let mr = document.getElementById("button4");
+// let p = document.querySelector("p");
+
+// m.addEventListener("click",()=>{
+//     p.textContent=`Perseverance rover – the largest, most advanced rover NASA has sent to another world – touched down on Mars on Feb. 18, 2021, after a 203-day journey traversing 293 million miles (472 million kilometers). The Ingenuity helicopter rode to Mars attached to the belly of Perseverance.
+
+//     Perseverance is one of three spacecraft that arrived at Mars in 2021. The Hope orbiter from the United Arab Emirates arrived on Feb. 9, 2021. China’s Tianwen-1 mission arrived on Feb. 10, 2021, and includes an orbiter, a lander, and a rover. Europa and India also have spacecraft studying Mars from orbit.
+    
+//     In May 2021, China became the second nation to ever land successfully on Mars when its Zhurong Mars rover touched down.`;
+    
+// })
+// e.addEventListener("click",()=>{
+//    p.textContent=`Storytellers explore the nature of our planet and possible alternate realities in many books, movies, and television shows. The iconic film "Planet of the Apes" (and its sequels) takes place in a future in which astronauts "discover" a planet inhabited by highly intelligent apes and primitive humans, only to realize later, much to their dismay, that – spoiler alert! – it was Earth all along.
+
+//    In the long-running and re-booted television series "Battlestar Galactica" – tired survivors of a war with highly evolved robots called Cylons are on a quest to find Earth, a long-lost colony.
+   
+//    In other stories, Earth has been abandoned or destroyed, such as in the Joss Whedon series "Firefly," or the book and its film adaptation "The Hitchhiker's Guide to the Galaxy." In the animated feature "Titan A.E." – Earth has been destroyed by an alien species, but a well-placed planet builder recreates it and all the species that live on it.`;
+// })
+// v.addEventListener("click",()=>{
+//     p.textContent=`The Venera 3 to 6 probes were similar. Weighing approximately one ton, and launched by the Molniya-type booster rocket, they included a cruise "bus" and a spherical atmospheric entry probe. The probes were optimised for atmospheric measurements, but not equipped with any special landing apparatus. Although it was hoped they would reach the surface still functioning, the first probes failed almost immediately, thereby disabling data transmission to Earth.
+
+//     Venera 3 became the first human-made object to impact another planet's surface as it crash-landed on 1 March 1966. However, as the spacecraft's data probes had failed upon atmospheric penetration, no data from within the Venusian atmosphere were retrieved from the mission.
+    
+//     On 18 October 1967, Venera 4 became the first spacecraft to measure the atmosphere of another planet. While the Soviet Union initially claimed the craft reached the surface intact, re-analysis, including atmospheric occultation data from the American Mariner 5 spacecraft that flew by Venus the day after its arrival, demonstrated that Venus's surface pressure was`;
+// })
+// mr.addEventListener("click",()=>{
+//    p.textContent=`Along with Venus, Earth, and Mars, Mercury is one of the rocky planets. It has a solid surface that is covered with craters. It has no atmosphere, and it doesn’t have any moons. Mercury likes to keep things simple.
+
+//    This small planet spins around slowly compared to Earth, so one day lasts a long time. Mercury takes 59 Earth days to make one full rotation. A year on Mercury goes by fast. Because it’s the closest planet to the sun, it doesn’t take very long to go all the way around. It completes one revolution around the sun in just 88 Earth days. If you lived on Mercury, you’d have a birthday every three months!
+   
+//    A day on Mercury is not like a day here on Earth. For us, the sun rises and sets each and every day. Because Mercury has a slow spin and short year, it takes a long time for the sun to rise and set there. Mercury only has one sunrise every 180 Earth days! Isn't that weird?
+   
+//    `;
+// })
+
+
+
+2.// let display = document.getElementById("text");
+// let inputs = document.querySelectorAll(".input");
+// let clear = document.getElementById("clear");
+// let result = document.getElementById("result");
+
+// inputs.forEach(el=>el.addEventListener("click",()=>{
+//     display.textContent+=el.textContent
+// }))
+
+// clear.addEventListener("click",()=>{
+//     display.textContent=""
+// })
+
+// result.addEventListener("click", ()=>{
+//     display.textContent=eval(display.textContent);
+// })
+
